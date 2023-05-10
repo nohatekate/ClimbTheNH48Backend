08c1e776214279ea157266e7b8eab8fb3184c617
nohatekate <kateiscoding@gmail.com>
2023-05-09 19:40:59 -0500

    hike router and routes stubbed out

--- routes/hike-router.js
+++ routes/hike-router.js
@@ -1,4 +1,4 @@
-// ./routes/people-router.js
+
 
 
 // DEPENDENCIES
@@ -12,15 +12,33 @@ const router = express.Router()
 
 
 // HIKE INDEX ROUTE
-router.get("/", async (req, res) => {
+router.get("/", (req, res, next) => {
 	res.status(200).json({message: "HIKE index route"})
 });
 
 
 // HIKE CREATE ROUTE
-router.post("/", async (req, res) =>  {
+router.post("/", (req, res, next) =>  {
+    console.log("test data", req.body)
 	res.status(201).json({message: "HIKE create route"})
 });
+
+//HIKE SHOW ROUTE
+router.get("/:id", (req, res, next) => {
+	res.status(200).json({message: "HIKE detail route", resource: req.params.id})
+});
+
+//HIKE SHOW ROUTE
+router.delete("/:id", (req, res, next) => {
+	res.status(200).json({message: "HIKE destroy route", resource: req.params.id})
+});
+
+//HIKE UPDATE ROUTE
+router.put("/:id", (req, res, next) => {
+    console.log(req.body)
+	res.status(200).json({message: "HIKE update route", resource: req.params.id})
+});
+
 
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-
+const hikeRouter = require('./routes/hike-router')
 // DEPENDENCIES
-
 
 // initialize .env variables
 require("dotenv").config();
-
 require('./config/db.connection.js')
 
 // pull PORT from .env, give default value of 4000 and establish DB Connection
@@ -12,9 +10,20 @@ const { PORT } = process.env;
 
 // import express
 const express = require("express");
+const cors = require("cors")
+const morgan = require("morgan")
 
 // create application object
 const app = express();
+
+//MIDDLEWARE
+app.use(express.urlencoded({extended:true}))
+app.use(express.json()); // parse json bodies - this will run before our request accesses the people router
+app.use(cors());
+app.use(morgan("dev"))
+
+//ROUTER MIDDLEWARE
+app.use('/hike', hikeRouter)
 
 
 // ROUTES
